b4be0ed46f82f1151d1de6666b4f9d20b9ca1e18
Tom Peace <thomasjpeace29@gmail.com>
2025-12-04 11:09:17 +0000

    feat(repositories): implement the missing repositories for relationship, comment and like

---
+++ src/main/java/com/makersacademy/acebook/repository/RelationshipRepository.java
@@ -0,0 +1,8 @@
+package com.makersacademy.acebook.repository;
+
+import com.makersacademy.acebook.model.Relationship;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RelationshipRepository extends CrudRepository<Relationship, Long> {
+
+}
