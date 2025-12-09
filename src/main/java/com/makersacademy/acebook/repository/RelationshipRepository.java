e0349dcd09a6bc675b7e61cf1575f36bab02f241
Ollie Jeffcote <oliverjeffcote@gmail.com>
2025-12-09 21:16:50 +0000

    more

--- src/main/java/com/makersacademy/acebook/repository/RelationshipRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/RelationshipRepository.java
@@ -5,7 +5,6 @@ import com.makersacademy.acebook.model.Relationship;
 import com.makersacademy.acebook.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
